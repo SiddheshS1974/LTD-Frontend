5d3bd2fde36abd11c44b94ce5d4c83ded4d68e05
SiddheshS1974 <siddheshsk1706@gmail.com>
2026-06-13 00:14:28 -0400

    Update Step 4 training video to new Vimeo link

--- src/Step4.jsx
+++ src/Step4.jsx
@@ -108,12 +108,12 @@ export default function Step4() {
         <div style={{ width: "100%", maxWidth: "720px" }}>
           <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
             <iframe
-              src="https://player.vimeo.com/video/1068276039?badge=0&autopause=0&player_id=0&app_id=58479"
+              src="https://player.vimeo.com/video/1196162123?badge=0&autopause=0&player_id=0&app_id=58479"
               frameBorder="0"
               allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
               style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
-              title="BOP US Feb 2025"
+              title="Sample Video for LTD 2.0 Testing"
             />
           </div>
         </div>
